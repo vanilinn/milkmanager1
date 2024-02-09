d8a198522cdd36899a6e6ef2976c851c6a7cd203
i.petuhov <i.petuhov@uniyar.ac.ru>
2024-02-09 17:48:01 +0300

    added css and bootstrap

--- milkmanager/milkinfo/views.py
+++ milkmanager/milkinfo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Cistern, FillHistory
 
@@ -6,22 +6,26 @@ from .models import Cistern, FillHistory
 def index(request):
     cisterns = Cistern.objects.all()
     fill_history = FillHistory.objects.all().order_by('filled_at')
-    # print(1)
     return render(request, 'milkinfo/index.html', {'cisterns': cisterns, 'fill_history': fill_history})
 
 
 def fill_milk(request):
-
     if request.method == 'POST':
+        # имя из формы
         name = request.POST['name']
+        # объем из формы
         volume = int(request.POST['volume'])
+        # сортируем цистерны по текущему объему и выбираем наименее заполненную
         cistern = Cistern.objects.order_by('current_volume').first()
+        # если можем в нее долить, то доливаем
         if cistern.current_volume + volume <= cistern.volume:
             cistern.current_volume += volume
             cistern.save()
+            # сохраняем запись о заливке
             FillHistory.objects.create(cistern=cistern, filler_name=name, filled_volume=volume)
-            return JsonResponse({'success': True, 'cistern_id': cistern.id, 'cistern_volume': cistern.current_volume})
+            return JsonResponse({'success': True, 'cistern_name': cistern.name, 'cistern_volume': cistern.current_volume,
+                                 'milk_filled': volume, 'name': name})
         else:
             return JsonResponse(
-                {'success': False, 'error_message': 'Невозможно долить молоко, не хватает места в канистре'})
-    return render(request, 'milkinfo/form.html')  # , {'name': name, 'volume': volume})
+                {'success': False, 'error_message': 'Невозможно долить молоко, не хватает места в цистерне'})
+    return render(request, 'milkinfo/form.html')
--- milkmanager/static/js/ajax.js
+++ milkmanager/static/js/ajax.js
@@ -1,31 +1,28 @@
-$(document).ready(function() {
-    $('#fillForm').submit(function(e) {
+$(document).ready(function () {
+    $('#fillForm').submit(function (e) {
         e.preventDefault();
         let form = $(this);
         const csrf = $('input[name=csrfmiddlewaretoken]').val();
         let name = $('input[name=name]').val();
         let volume = $('input[name=volume]').val();
-        console.log('csrf ->', csrf);
-        console.log('name ->', name, volume);
+        //console.log('csrf ->', csrf);
+        //console.log('name ->', name, volume);
         $.ajax({
             type: 'POST',
             url: 'fill_milk/',
-            // url: '/',
             data: {
-            'csrfmiddlewaretoken': csrf,
-            'name': name,
-            'volume': volume,
-        },
-            success: function(response) {
+                'csrfmiddlewaretoken': csrf,
+                'name': name,
+                'volume': volume,
+            },
+            success: function (response) {
                 if (response.success) {
-                    console.log('send message ->', response);
+                    //console.log('send message ->', response.cistern_name);
                     $('#fillMessage').text('Молоко успешно налито');
-                    $(`#${response.cistern_id}`).text(response.cistern_id  + ' - ' + response.cistern_volume + 'L / 300L');
-
-
-
+                    //перерисовываем цистерну и добавляем в историю запись
+                    $(`#${response.cistern_name}`).text(response.cistern_name + ' заполнена на  ' + response.cistern_volume + ' л. из 300 л.');
+                    $('#fill_history_table').append('<li>' + response.name + ' залил ' + response.milk_filled + ' л. в ' + response.cistern_name + '</li>');
                     form.trigger('reset');
-                    // location.reload(); // Обновляем страницу после успешного залива
                 } else {
                     $('#fillMessage').text(response.error_message);
                 }
